cb61ecd2c98b4869c04eeee9018a05617ab3cb2c
Nguyễn Văn Huy <77230707+NguyenVanHuy2106@users.noreply.github.com>
2025-08-05 11:35:45 +0700

    update api live

--- src/config/APIToken.js
+++ src/config/APIToken.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = "https://service.vietnamluxtour.com/api";
 
 const APIToken = axios.create({
   baseURL: API_BASE_URL,
