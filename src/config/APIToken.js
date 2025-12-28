2b66071be8db130a9f20bbe3526ab238551dfa1d
Nguyễn Văn Huy <77230707+NguyenVanHuy2106@users.noreply.github.com>
2025-12-28 12:27:10 +0700

    up server

--- src/config/APIToken.js
+++ src/config/APIToken.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = "https://service.myvietnamtour.vn/api";
 
 const APIToken = axios.create({
   baseURL: API_BASE_URL,
