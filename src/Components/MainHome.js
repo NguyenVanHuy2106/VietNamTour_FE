85f6e9d1df296d2aaf35a7ab0141f6bc45f496df
Nguyễn Văn Huy <77230707+NguyenVanHuy2106@users.noreply.github.com>
2026-09-16 16:08:09 +0700

    Update canonical mainhome

--- src/Components/MainHome.js
+++ src/Components/MainHome.js
@@ -1,67 +1,94 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { Helmet } from "react-helmet";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import FadeInSection from "./FadeInSection";
-// import FloatingButtons from "./FloatingButton";
 import FamousDes from "../app/FamousDes";
 
 import "./Styles/main-home.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Carousel } from "react-bootstrap";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 import Search from "../app/Search";
-import NewTour from "../app/NewTour";
 import CompanyTour from "../app/CompanyTour";
 import Customer from "../app/Customer";
 import CustomerFeedBack from "../app/CustomerFeedBack";
-import Collections from "../app/Collections";
 import Banner from "../app/Banner";
 import Relation from "../app/Relation";
 import HomeCTA from "../app/HomeCTA";
 
-const MainHome = ({ history }) => {
+const MainHome = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
   return (
     <div className="App" style={{ margin: 0 }}>
+      <Helmet>
+        <title>
+          Việt Nam Tour - Tổ chức Tour du lịch & Sự kiện chuyên nghiệp
+        </title>
+
+        <meta
+          name="description"
+          content="Việt Nam Tour chuyên tổ chức tour du lịch trong nước, tour đoàn doanh nghiệp, MICE, team building, gala dinner và sự kiện trọn gói."
+        />
+
+        <link rel="canonical" href="https://myvietnamtour.vn/" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Việt Nam Tour" />
+
+        <meta
+          property="og:title"
+          content="Việt Nam Tour - Tour du lịch & Sự kiện chuyên nghiệp"
+        />
+
+        <meta
+          property="og:description"
+          content="Chuyên tổ chức tour du lịch, tour đoàn doanh nghiệp, team building, MICE, gala dinner và sự kiện trọn gói."
+        />
+
+        <meta property="og:url" content="https://myvietnamtour.vn/" />
+
+        <meta
+          property="og:image"
+          content="https://cdn.myvietnamtour.vn/uploads/1.png"
+        />
+      </Helmet>
+
       <FadeInSection>
         <Banner />
       </FadeInSection>
 
-      {/* <FadeInSection>
-        <NewTour />
-      </FadeInSection> */}
       <FadeInSection>
         <CompanyTour />
       </FadeInSection>
+
       <FadeInSection>
         <Search />
       </FadeInSection>
+
       <FadeInSection>
         <Relation />
       </FadeInSection>
+
       <FadeInSection>
         <FamousDes />
       </FadeInSection>
+
       <FadeInSection>
         <Customer />
       </FadeInSection>
+
       <FadeInSection>
         <CustomerFeedBack />
       </FadeInSection>
+
       <FadeInSection>
         <HomeCTA />
       </FadeInSection>
-      {/* <FadeInSection>
-        <Collections />
-      </FadeInSection> */}
-      {/* <FloatingButtons /> */}
     </div>
   );
 };
